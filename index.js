f640049bfbbebb89b779277f605b6798dfd6d886
Jerry Lee <lee@idoctor.co.jp>
2019-09-09 19:09:23 +0900

    add github gist

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const ClipyMate = require('clipy-mate-core');
 
 const ReadLine = require('./lib/readline');
+const GitHubGist = require('./lib/github');
+const Utils = require('./lib/utils');
 
 const clipy = new ClipyMate();
 const readline = new ReadLine(clipy);
 
-readline.updateSettings().then()
+(async() => {
+  const vaildRes = await readline.validateSettings();
+  const settingsPath = readline.opt.clipyMateCliSettingsPath;
 
-// console.log(clipy.opt);
+  if (!vaildRes.clipyMateCliPath) {
+    await readline.updateSettings();
+  }
+  const settings = await Utils.loadSettings(settingsPath);
+
+  const gist = new GitHubGist(settings);
+  const res = await gist.readGist();
+
+  console.log(res);
+
+  clipy.disconnect();
+  process.exit(0);
+})();
